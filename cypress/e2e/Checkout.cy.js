4acce282f5c3b409c52173762619b76dc50cc068
Humayun0008 <hmushtaq0008@gmail.com>
2023-05-02 00:10:51 +0500

    Added Test case "Verify User is Successfully Login before Checkout"

--- cypress/e2e/Checkout.cy.js
+++ cypress/e2e/Checkout.cy.js
@@ -22,4 +22,15 @@ describe('Place Order', () => {
         cy.Payment_Method();
         cy.Delete_Account();
     })
+    it("Verify User is Successfully Login before Checkout",()=>{
+        cy.NewResiter_User(Random_Email,Password);
+        cy.Login(Random_Email,Password);
+        cy.get(".navbar-nav >:nth-child(10)").should("have.text"," Logged in as "+user_name);
+        cy.Add_Product_in_Cart();
+        cy.Validate_Cart_Product();
+        cy.Checkout_Page();
+        cy.Payment_Method();
+        cy.Delete_Account();
+    })
+
 })
